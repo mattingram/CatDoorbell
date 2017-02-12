691425448a01900599cff2492500c5e08fa4b8b8
mattingram <mingram@vt.edu>
2017-02-12 12:33:11 -0500

    MI - Adding other people's code

--- CatDoorbell.ino
+++ CatDoorbell.ino
@@ -1,44 +1,60 @@
-int speakerPin = 3;
-int pirPin = 7;
-int ledPin = 13;
+// Uses a PIR sensor to detect movement, buzzes a buzzer
+// more info here: http://blog.makezine.com/projects/pir-sensor-arduino-alarm/
+// email me, John Park, at jp@jpixl.net
+// based upon:
+// PIR sensor tester by Limor Fried of Adafruit
+// tone code by michael@thegrebs.com
+
  
-int minSecsDelay = 2500;
-bool enableSound = false;
-long lastSend = millis() - minSecsDelay;
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 7;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+int pinSpeaker = 3;           //Set up a speaker on a PWM pin (digital 9, 10, or 11)
 
-void setup()
-{
-  pinMode(pirPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+  pinMode(pinSpeaker, OUTPUT);
   Serial.begin(9600);
-  if (enableSound) alarm();
 }
 
-void alarm()
-{
-  tone(speakerPin, 1060);
-  delay(300);
-  tone(speakerPin, 860);
-  delay(600);
-  noTone(speakerPin);
-}
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    if (enableSound) alarm();
+    delay(150);
 
-void loop()
-{
-  long now = millis();
-  if (digitalRead(pirPin) == HIGH)
-  {
-    if (now > (lastSend + minSecsDelay))
-    {
-      Serial.println(now);
-      digitalWrite(ledPin, HIGH);
-      if (enableSound){
-        alarm();
-      }
-      else
-        delay(500);
-      lastSend = now;
-    } 
+    
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+    }
+  } else {
+      digitalWrite(ledPin, LOW); // turn LED OFF
+      playTone(0, 0);
+      delay(300);    
+      if (pirState == HIGH){
+      // we have just turned off
+      Serial.println("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
   }
-  digitalWrite(ledPin, LOW);
+}
+// duration in mSecs, frequency in hertz
+void playTone(long duration, int freq) {
+    duration *= 1000;
+    int period = (1.0 / freq) * 1000000;
+    long elapsed_time = 0;
+    while (elapsed_time < duration) {
+        digitalWrite(pinSpeaker,HIGH);
+        delayMicroseconds(period / 2);
+        digitalWrite(pinSpeaker, LOW);
+        delayMicroseconds(period / 2);
+        elapsed_time += (period);
+    }
 }
